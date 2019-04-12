030e3c574c83ee9b727143602894e8277f16fdf0
Josip Tisaj <josip.tisaj00@gmail.com>
2019-04-12 20:41:38 +0200

    Finished Zad3

--- Lecture_01/Projects/Predavanje 1/Uvod_u_Kotlin/src/exercises/Zad3.kt
+++ Lecture_01/Projects/Predavanje 1/Uvod_u_Kotlin/src/exercises/Zad3.kt
@@ -1,37 +1,32 @@
-package exercises
-
 /*
 Implement the member functions of the Robot class. A Robot starts in the (0, 0) cell and can move to the right, to the left, down and up.
 Its location is stored as a pair of coordinates (x, y), where (0, 0) is the top-left corner:
-
 (x,y)
 (0,0) (1,0)
 (0,1) (1,1)
 Going right increases x coordinate, going down increases y coordinate, going left and up decreases x and y coordinates accordingly.
-
 Implement goRight(), goLeft(), goUp(), goDown() and getLocation() member functions. getLocation() method should return the coordinates as a string (x, y).
  */
-
 class Robot {
-    /*TODO*/
+    private var x: Int = 0
+    private var y: Int = 0
 
     fun goRight(steps: Int) {
-        TODO()
+        x+=steps
     }
 
     fun goLeft(steps: Int) {
-        TODO()
+        x-=steps
     }
 
     fun goDown(steps: Int) {
-        TODO()
+        y+=steps
     }
 
     fun goUp(steps: Int) {
-        TODO()
+        y-=steps
     }
-
-    fun getLocation(): String = TODO()
+    fun getLocation(): String = "( $x , $y)"
 }
 
 fun main(args: Array<String>) {
